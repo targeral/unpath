bc92c6ceead47c157576994fabc0a9c14e927017
targeral <2654955040@qq.com>
2022-04-25 23:19:16 +0800

    chore: update scripts and publish

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,10 @@
+import path from 'path';
+import fs from 'fs';
 import esbuild from 'esbuild';
 import { config } from './config.js';
+import { dirname } from './utils.js';
+
+fs.rmSync(path.join(dirname(), '../dist'), {recursive: true, force: true});
 
 esbuild.buildSync({
     ...config,
